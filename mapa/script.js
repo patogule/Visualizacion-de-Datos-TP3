ab957575459fe307210fed91b26ce055a37146a6
Patricio Guledjian <patogulee@gmail.com>
2023-04-14 13:52:58 -0300

    Mapa con los barrios y sus categorias maximas

---
+++ mapa/script.js
@@ -0,0 +1,62 @@
+
+const mapaFetch = d3.json('barrios-caba.geojson');
+const dataFetch = d3.dsv(';', '147_intoxicacion_alimento.csv', d3.autoType);
+
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
+  /* Agrupamos reclamos x barrio y categoria */
+  const reclamosPorBarrioYCateg = d3.rollup(data, 
+    v => d3.rollup(v, 
+      g => g.length, 
+      d => d.categoria
+    ), 
+    d => d.domicilio_barrio
+  );
+  console.log(reclamosPorBarrioYCateg)
+  /* Obtenemos las categorías únicas del conjunto de datos */
+  const categoriasUnicas = Array.from(new Set(data.map(d => d.categoria)));
+  console.log(categoriasUnicas)    
+ 
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+    color: {
+      // Cantidad de denuncias por categoria
+      type: 'categorical',
+      domain: categoriasUnicas,
+      scheme: 'dark2',
+      label: 'Categoría con más denuncias',
+      legend: true,
+    },
+    marks: [
+      Plot.geo(barrios, {
+        fill: d => {
+          let nombreBarrio = d.properties.BARRIO;
+          let reclamosPorCategoria = reclamosPorBarrioYCateg.get(nombreBarrio);
+          let reclamosPorCategoriaArray = Array.from(reclamosPorCategoria, ([categoria, valor]) => [categoria, valor]);
+          let categoriaMaxDenuncias = d3.max(reclamosPorCategoriaArray, d => d[1]);
+          let categoriaMaxDenunciasNombre = reclamosPorCategoriaArray.find(d => d[1] === categoriaMaxDenuncias)[0];
+          console.log(categoriaMaxDenunciasNombre)
+          return categoriaMaxDenunciasNombre;
+        },
+        stroke: '#ddd',
+        title: (d) => `${d.properties.BARRIO}`,
+      }),
+      Plot.geo(barrios, {
+        href: () => 'https://es.wikipedia.org/wiki/Palermo_(Buenos_Aires)',
+        stroke: 'black',
+        strokeWidth: 2,
+        fill: 'transparent',
+        title: d => d.properties.BARRIO,
+        filter: d => d.properties.BARRIO == 'PALERMO'
+      }),
+    ],
+  });
+  /* Agregamos al DOM la visualización chartMap */
+  console.log('chartMap', chartMap);
+
+  d3.select('#chart').append(() => chartMap);
+});
